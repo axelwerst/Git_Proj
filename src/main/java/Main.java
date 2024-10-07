a2195869015b046cff2ce26311fb9cb2354105d8
Oleksandr Honcharenko <axelwerst@gmail.com>
2024-10-07 18:26:20 +0200

    random

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello CODILlAS :-)");
+        Random ran = new Random();
+        int nxt = ran.nextInt(11);
+System.out.println("твоє счастливе число " + nxt);
     }
 }
